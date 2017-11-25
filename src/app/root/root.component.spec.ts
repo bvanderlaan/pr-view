10ff3581457a37f4201d6d7f7f51c1a7754b2f2d
Brad van der Laan <bvanderl@opentext.com>
2017-11-24 22:34:01 -0500

    Update Home and Root tests. I had not updated the Root tests since the start of the project so it was still expecting the Root component to render a title. This responsability was moved to the Home component but the home component did not contain tests to verify that.
    This change set moves the tests from the Root to the Home component and updates them.
    
    These two components probably don't have good coverage but at least they are passing now.

--- src/app/root/root.component.spec.ts
+++ src/app/root/root.component.spec.ts
@@ -1,13 +1,14 @@
 import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
+import { LayoutModule } from '../layout';
 import { RootComponent } from './root.component';
 
 describe('RootComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        RootComponent
-      ],
+      declarations: [ RootComponent ],
+      imports: [ RouterTestingModule, LayoutModule ]
     }).compileComponents();
   }));
 
@@ -15,18 +16,5 @@ describe('RootComponent', () => {
     const fixture = TestBed.createComponent(RootComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  }));
-
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(RootComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(RootComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!!');
   }));
 });
